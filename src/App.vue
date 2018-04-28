695d8943cd4ebe765fe15f39593f2a0883d444f0
rxq <894058740@qq.com>
2018-04-28 17:27:31 +0800

    vue+ssr
    vue+ssr

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div id="text">{{text}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'RXQ'
+        }
+    }
+}
+</script>
+
+<style>
+#text{
+    color: red;
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by hp on 2018/4/28.
+ */
+import Vue from 'vue';
+import App from './App.vue';
+
+import './assets/styles/test.css';
+import './assets/styles/test-stylus.styl';
+import './assets/images/bg.jpeg';
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+new Vue({
+    render: (h) => h(App)
+}).$mount(root)
